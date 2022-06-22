f309955bddb99a7ef5ea18fbf70f305a8de45312
Matthieu Riegler <kyro38@gmail.com>
2022-06-23 00:01:30 +0200

    Update eslint to add no-constant-binary-expression

--- src/app/geo/components/card/card.component.ts
+++ src/app/geo/components/card/card.component.ts
@@ -1,11 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
 })
-export class CardComponent implements OnInit {
-  constructor() {}
-
-  ngOnInit() {}
-}
+export class CardComponent {}
