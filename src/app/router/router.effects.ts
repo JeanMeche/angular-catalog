08e74d00f6195c053011b0677de66ef672cf7b15
Matthieu Riegler <kyro38@gmail.com>
2022-09-27 00:56:20 +0200

    Deps update

--- src/app/router/router.effects.ts
+++ src/app/router/router.effects.ts
@@ -3,12 +3,13 @@ import { createEffect } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
 import { filter, map } from 'rxjs';
 import { GeoActions } from '../geo/store/geo.action';
+import { GeoState } from '../geo/store/geo.reducer';
 import { isDefined } from '../helpers/helpers';
 import { selectRouteParam } from './router.selector';
 
 @Injectable({ providedIn: 'root' })
 export class RouterEffects {
-  constructor(private readonly store: Store<unknown>) {}
+  constructor(private readonly store: Store<GeoState>) {}
 
   loadCommune$ = createEffect(() => {
     return this.store.select(selectRouteParam('codeCommune')).pipe(
